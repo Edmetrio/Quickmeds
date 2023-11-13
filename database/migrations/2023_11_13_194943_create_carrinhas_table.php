e644aee89cd53d638a7b49b92f4cc306f04cec29
Edmétrio J <edmetrio06@gmail.com>
2023-11-13 21:59:47 +0200

    database carrinha

---
+++ database/migrations/2023_11_13_194943_create_carrinhas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carrinha', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('produto_id')->nullable();
+            $table->foreign('produto_id')->references('id')->on('produto')->onDelete('cascade')->onUpdate('cascade');
+            $table->uuid('endereco_id')->nullable();
+            $table->foreign('endereco_id')->references('id')->on('endereco')->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('quantidade', 20,2)->nullable();
+            $table->string('transporte')->default('true');
+            $table->string('estado')->default('on');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carrinha');
+    }
+};
